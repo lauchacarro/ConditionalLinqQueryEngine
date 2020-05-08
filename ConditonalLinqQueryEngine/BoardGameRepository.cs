f53c663899586b32c99c47df91810ccfdd41d41a
lautaro <lautaroecarro@gmail.com>
2020-05-08 20:44:19 -0300

    Remove Using's

--- ConditonalLinqQueryEngine/BoardGameRepository.cs
+++ ConditonalLinqQueryEngine/BoardGameRepository.cs
@@ -1,8 +1,6 @@
-﻿using ConditonalLinqQueryEngine.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+
+using ConditonalLinqQueryEngine.Models;
 
 namespace ConditonalLinqQueryEngine
 {
